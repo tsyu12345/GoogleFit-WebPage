42bcee90596222c77213601c7de9ea40a01e7cd7
SO 高林秀 <s.takabayashi@super-one.co.jp>
2022-12-02 20:01:14 +0900

    【ホーム画面制作】useSatetとuseEffectについて学習。仮でサーバーAPIからアイコンのURLを取得する処理を実装

--- src/Components/Header/Components/UserIcon.tsx
+++ src/Components/Header/Components/UserIcon.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-type Props = {
-    
-
+/**
+ * @returns iconのURL
+ */
+const requestIcon = async ():Promise<string> => {
+    const response = await fetch("some url");
+    const data = await response.json();
+    return data; //todo:暫定
 };
 
-export const UserIcon = (props: Props): JSX.Element => {
+export const UserIcon = (): JSX.Element => {
+    const [iconUrl, getUrl] = useState<string>(undefined);
+    useEffect(() => {
+        console.log("useEffect!");
+        requestIcon().then((url) => {
+            getUrl(url);
+        }).catch((error) => {
+            throw new Error("ユーザーアイコン画像取得でエラーが発生しました", error);
+        });
+    });
+
     return (
         <div className="mt-auto mr-4 mb-auto ml-auto border-solid border-gray-500 border-3 rounded-full h-10 w-10 bg-gray-300">
-            {/*todo : add user icon here */}
+            <img src={iconUrl} className="h-full w-full rounded-full" />
         </div>
     );
 };
